d5413817085a74f6d3eee734bec13faa17ee7580
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-20 18:40:35 +0200

    home as test for password game, to move on password game screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
+import { View, Text, Button } from 'react-native';
+import { Card, Body, CardItem } from 'native-base'
+import { AppProps } from '../../App';
+import { PasswordsGame } from '../modules/Passwords';
 
-interface HomeProps {
+interface HomeProps extends AppProps {
 }
 interface HomeState {
+    pwdGame: PasswordsGame
 }
 
 export class Home extends Component<HomeProps, HomeState> {
@@ -10,16 +15,52 @@ export class Home extends Component<HomeProps, HomeState> {
     constructor(props: HomeProps) {
         super(props)
         this.state = {
+            pwdGame: new PasswordsGame([
+                ['a', 'b', 'x'],
+                ['f', 'd'],
+                ['t']
+            ])
         }  
     }
 
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    static navigationOptions = {
+        title: 'Home',
+    };
+
+    renderPwdPossibilities(){
+        const strings = this.state.pwdGame.getSubStringsPossibles()
+        return strings && strings.map( (p, i) => (
+            <Text key={i}>{p}</Text>
+        ))
+    }
+    
+    renderPwdPossibleWords(){
+        const strings = this.state.pwdGame.getSolutions()
+        return strings && strings.map( (p, i) => (
+            <Text key={i}>{p}</Text>
+        ))
+    }
+
+    render() {
+        return (
+        <View>
+            {/* <Button
+                title='Go to game selector'
+                onPress={() => this.props.navigation.navigate('GameSelector')} 
+            /> */}
+            <Card>
+                <CardItem>
+                    <Body>
+                        {this.renderPwdPossibilities()}
+                    </Body>
+                </CardItem>
+            </Card>
+            <Card>
+                {this.renderPwdPossibleWords()}
+            </Card>
+        </View>
+        )
+    }
 }
 
 export default Home
