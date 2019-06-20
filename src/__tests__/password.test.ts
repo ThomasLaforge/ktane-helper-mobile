981c6cd9f3c3170755d57a02ebd91391c7ccbbec
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-20 08:42:19 +0200

    add jest

---
+++ src/__tests__/password.test.ts
@@ -0,0 +1,13 @@
+import { PasswordsGame } from "../modules/Passwords";
+
+describe('password', () => {
+    test('get all string possibilities', () => {
+        let pwGame = new PasswordsGame([
+            ['a', 'b'],
+            // ['c', 'd']
+        ])
+        const possibilitiesStrings = pwGame.getSubStringsPossibles()
+        console.log('possibilitiesStrings', possibilitiesStrings)
+        expect(possibilitiesStrings).toEqual(['ac', 'ad', 'bc', 'bd'])
+    })
+})
