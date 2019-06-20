af60775f0395fe19d74c6cf3cd6b4160623094eb
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-21 00:48:29 +0200

    complexe strings game

---
+++ src/modules/ComplexStrings.ts
@@ -0,0 +1,82 @@
+export enum ComplexStringColor {
+    Blue,
+    Red,
+    RedAndBlue,
+    Any
+}
+
+export enum ComplexStringLetter {
+    C,
+    D,
+    S,
+    P,
+    B
+}
+
+export class ComplexStringsGame {
+    constructor(
+        public hasParralelPort: boolean,
+        public lastSerialNumberDigitIsEven: boolean, //Dernier chiffre est pair
+        public hasMoreThanOneBattery: boolean
+    ){}
+
+    resolveString(stringColor:ComplexStringColor, hasStar: boolean, ledOn: boolean){
+        const letter = this.getLetter(stringColor, hasStar, ledOn)
+
+        switch (letter) {
+            case ComplexStringLetter.C:
+                return true
+            case ComplexStringLetter.D:
+                return false
+            case ComplexStringLetter.S:
+                return this.lastSerialNumberDigitIsEven
+            case ComplexStringLetter.P:
+                return this.hasParralelPort
+            case ComplexStringLetter.B:
+                return this.hasMoreThanOneBattery
+        }
+    }
+
+    getLetter(stringColor:ComplexStringColor, hasStar: boolean, ledOn: boolean){
+        // Color Any
+        if( stringColor === ComplexStringColor.Any && !hasStar && !ledOn )
+            { return ComplexStringLetter.C }
+        else if( stringColor === ComplexStringColor.Any && hasStar && !ledOn )
+            { return ComplexStringLetter.C }
+        else if( stringColor === ComplexStringColor.Any && !hasStar && ledOn )
+            { return ComplexStringLetter.D }
+        else if( stringColor === ComplexStringColor.Any && hasStar && ledOn )
+            { return ComplexStringLetter.B }
+
+        // Color Blue
+        else if( stringColor === ComplexStringColor.Blue && !hasStar && !ledOn )
+            { return ComplexStringLetter.S }
+        else if( stringColor === ComplexStringColor.Blue && hasStar && !ledOn )
+            { return ComplexStringLetter.D }
+        else if( stringColor === ComplexStringColor.Blue && !hasStar && ledOn )
+            { return ComplexStringLetter.P }
+        else if( stringColor === ComplexStringColor.Blue && hasStar && ledOn )
+            { return ComplexStringLetter.P }
+
+        // Color Red
+        else if( stringColor === ComplexStringColor.Red && !hasStar && !ledOn )
+            { return ComplexStringLetter.S }
+        else if( stringColor === ComplexStringColor.Red && hasStar && !ledOn )
+            { return ComplexStringLetter.C }
+        else if( stringColor === ComplexStringColor.Red && !hasStar && ledOn )
+            { return ComplexStringLetter.B }
+        else if( stringColor === ComplexStringColor.Red && hasStar && ledOn )
+            { return ComplexStringLetter.B }
+
+        // Color Red and Blue
+        else if( stringColor === ComplexStringColor.RedAndBlue && !hasStar && !ledOn )
+            { return ComplexStringLetter.S }
+        else if( stringColor === ComplexStringColor.RedAndBlue && hasStar && !ledOn )
+            { return ComplexStringLetter.P }
+        else if( stringColor === ComplexStringColor.RedAndBlue && !hasStar && ledOn )
+            { return ComplexStringLetter.S }
+        else if( stringColor === ComplexStringColor.RedAndBlue && hasStar && ledOn )
+            { return ComplexStringLetter.D }
+            
+    }
+}
