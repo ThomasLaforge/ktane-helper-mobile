6f0e8f3fca4a9371fab1d7092057431348e3570e
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-24 15:23:42 +0200

    button game UI only text mode

--- src/screens/Games/ButtonGame.tsx
+++ src/screens/Games/ButtonGame.tsx
@@ -15,3 +15,48 @@
  * - reste -> 1
  * 
  */
+import React, { Component } from 'react'
+import { View, Text, Button } from 'react-native';
+import { AppProps } from '../../../App';
+
+interface ButtonGameProps extends AppProps {
+}
+interface ButtonGameState {
+}
+
+export class ButtonGame extends Component<ButtonGameProps, ButtonGameState> {
+
+    constructor(props: ButtonGameProps) {
+        super(props)
+        this.state = {
+
+        }  
+    }
+
+    static navigationOptions = {
+        title: 'Bouton',
+    };
+
+    render() {
+        return (
+        <View>
+            <Text>
+            {`
+Il faut d'abord essayer de voir s'il s'agit d'un des trois cas rapides:
+    - 1 pile + Detonate
+    - 2 piles + FRK
+    - Hold + rouge
+
+Si ce n'est pas le cas alors il faut garder le bouton appuyer.
+En fonction de la couleur, il faut relacher quand un numéro est présent dans le décompte:
+    - bleu -> 4
+    - jaune -> 5
+    - reste -> 1
+            `}
+            </Text>
+        </View>
+        )
+    }
+}
+
+export default ButtonGame
