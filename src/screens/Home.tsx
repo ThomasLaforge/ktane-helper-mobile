3466b1b6e4ab080e370817dab5d1943eef283adc
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-24 15:26:16 +0200

    refreshing usual home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import { View, Text, Button } from 'react-native';
-import { Card, Body, CardItem } from 'native-base'
+import { Text, Button } from 'react-native';
+import { Container, Content } from 'native-base'
 import { AppProps } from '../../App';
-import { PasswordsGame } from '../modules/Passwords';
 
 interface HomeProps extends AppProps {
 }
 interface HomeState {
-    pwdGame: PasswordsGame
 }
 
 export class Home extends Component<HomeProps, HomeState> {
@@ -15,11 +13,7 @@ export class Home extends Component<HomeProps, HomeState> {
     constructor(props: HomeProps) {
         super(props)
         this.state = {
-            pwdGame: new PasswordsGame([
-                ['a', 'b', 'x'],
-                ['f', 'd'],
-                ['t']
-            ])
+
         }  
     }
 
@@ -27,38 +21,23 @@ export class Home extends Component<HomeProps, HomeState> {
         title: 'Home',
     };
 
-    renderPwdPossibilities(){
-        const strings = this.state.pwdGame.getSubStringsPossibles()
-        return strings && strings.map( (p, i) => (
-            <Text key={i}>{p}</Text>
-        ))
-    }
-    
-    renderPwdPossibleWords(){
-        const strings = this.state.pwdGame.getSolutions()
-        return strings && strings.map( (p, i) => (
-            <Text key={i}>{p}</Text>
-        ))
-    }
-
     render() {
         return (
-        <View>
-            {/* <Button
-                title='Go to game selector'
-                onPress={() => this.props.navigation.navigate('GameSelector')} 
-            /> */}
-            <Card>
-                <CardItem>
-                    <Body>
-                        {this.renderPwdPossibilities()}
-                    </Body>
-                </CardItem>
-            </Card>
-            <Card>
-                {this.renderPwdPossibleWords()}
-            </Card>
-        </View>
+        <Container>
+        <Content>
+            <Text>Welcome to ktane helper !</Text>
+            <Text>How can I help you ?</Text>
+            {/* <Button block info  */}
+            <Button title={'I\'m an Expert'}
+                onPress={() => this.props.navigation.navigate('GameSelector')}
+            />
+                {/* <Text>I'm an Expert</Text>
+            </Button> */}
+            <Button title={'I\'m the Defuser'}
+                onPress={() => this.props.navigation.navigate('DefuserHelp')}
+            />
+        </Content>
+        </Container>
         )
     }
 }
