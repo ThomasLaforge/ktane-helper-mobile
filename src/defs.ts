ad85b000af937bf6c44dc8e47592b75dfdf6f472
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-24 15:27:17 +0200

    games definition

---
+++ src/defs.ts
@@ -0,0 +1,49 @@
+import ButtonGame from "./screens/Games/ButtonGame";
+import Passwords from "./screens/Games/Passwords";
+import ComplexStrings from "./screens/Games/ComplexStrings";
+import { MazeGame } from "./modules/Maze";
+import { KnobGame } from "./modules/Knob";
+
+export const GAMES = {
+    Passwords: {
+        image: require('../assets/GamesLogos/Passwords.png'),
+        screen: Passwords
+    },
+    ButtonGame: {
+        image: require('../assets/GamesLogos/ButtonGame.png'),
+        screen: ButtonGame
+    },
+    ComplexStrings: {
+        image: require('../assets/GamesLogos/ComplexStrings.png'),
+        screen: ComplexStrings
+    },
+    Memory: {
+        image: require('../assets/GamesLogos/Memory.png'),
+    },
+    Simon: {
+        image: require('../assets/GamesLogos/Simon.png'),
+    },
+    Symbols: {
+        image: require('../assets/GamesLogos/Symbols.png'),
+    },
+    Knob: {
+        image: require('../assets/GamesLogos/Knob.png'),
+        screen: KnobGame
+    },
+    Maze: {
+        image: require('../assets/GamesLogos/Maze.png'),
+        screen: MazeGame
+    },
+    Morse: {
+        image: require('../assets/GamesLogos/Morse.png'),
+    },
+    Strings: {
+        image: require('../assets/GamesLogos/Strings.png'),
+    },
+    StringsSequence: {
+        image: require('../assets/GamesLogos/StringsSequence.png'),
+    },
+    WhoIsTheFirst: {
+        image: require('../assets/GamesLogos/WhoIsTheFirst.png')
+    }
+}
