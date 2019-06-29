6d528ec634d2daeedd20e08abb00dc24505a776d
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-29 12:52:21 +0200

    add font for native base

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import HomeScreen from './src/screens/Home'
 import GameSelectorScreen from './src/screens/GameSelector'
 import DefuserHelpScreen from './src/screens/DefuserHelp'
 import { createStackNavigator, createAppContainer, NavigationScreenProps, StackNavigatorConfig, NavigationRouteConfigMap } from 'react-navigation';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { GAMES } from './src/defs';
 
 export interface AppProps extends NavigationScreenProps {
@@ -35,10 +35,32 @@ const navigatorOptions: StackNavigatorConfig = {
 const MainNavigator = createStackNavigator(routes, navigatorOptions);
 const RootNavigator = createAppContainer(MainNavigator);
 
-export default RootNavigator
+// ----------------------------------------------------------------------------------------- //
 
-// export default class App extends Component {
-//   render() {
-//     return <Provider><RootNavigator /></Provider>
-//   }
-// }
+import { AppLoading, Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class App extends Component<{}, {fontLoaded: boolean}> {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      fontLoaded: false
+    }
+  }
+  
+  // Later on in your component
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ fontLoaded: true })
+  }
+  
+  render() {
+    // return <Provider><RootNavigator /></Provider>
+    return this.state.fontLoaded ? <RootNavigator /> : <AppLoading />
+  }
+}
