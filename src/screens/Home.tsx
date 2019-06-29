2bc8edd0b26085d6cb995641f8df4cb76db79b96
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-06-29 12:52:47 +0200

    styles for home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Text, Button } from 'react-native';
-import { Container, Content } from 'native-base'
+import { StyleSheet, Dimensions, View } from 'react-native';
+import { Text, Container, Content, Button } from 'native-base'
 import { AppProps } from '../../App';
 
 interface HomeProps extends AppProps {
@@ -25,17 +25,23 @@ export class Home extends Component<HomeProps, HomeState> {
         return (
         <Container>
         <Content>
-            <Text>Welcome to ktane helper !</Text>
-            <Text>How can I help you ?</Text>
+            <Text style={styles.h1}>Welcome to ktane helper !</Text>
+            <Text style={styles.helpSentence}>How can I help you ?</Text>
             {/* <Button block info  */}
-            <Button title={'I\'m an Expert'}
-                onPress={() => this.props.navigation.navigate('GameSelector')}
-            />
-                {/* <Text>I'm an Expert</Text>
-            </Button> */}
-            <Button title={'I\'m the Defuser'}
-                onPress={() => this.props.navigation.navigate('DefuserHelp')}
-            />
+            <View style={styles.buttons}>
+                <Button block info
+                    style={styles.buttonsElt}
+                    onPressIn={() => this.props.navigation.navigate('GameSelector')}
+                >
+                    <Text>I'm an Expert</Text>
+                </Button>
+                <Button block info
+                    style={styles.buttonsElt}
+                    onPressIn={() => this.props.navigation.navigate('DefuserHelp')}
+                >
+                    <Text>I'm the Defuser</Text>
+                </Button>
+            </View>
         </Content>
         </Container>
         )
@@ -43,3 +49,23 @@ export class Home extends Component<HomeProps, HomeState> {
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+    h1: {
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginVertical: 20
+    },
+    helpSentence: {
+        textAlign: 'center',
+        marginVertical: 30
+    },
+    buttons: {
+        paddingHorizontal: 20
+    },
+    buttonsElt: {
+        marginVertical: 30,
+        // width: Math.floor(Dimensions.get('screen').width * 80 / 100)
+    }
+})
